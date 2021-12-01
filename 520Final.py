27a6beb9d07b5d67a432195e6da5ef2ab4dd4d92
grcosta14 <66080636+grcosta14@users.noreply.github.com>
2021-12-01 12:33:56 -0500

    Add files via upload

---
+++ 520Final.py
@@ -0,0 +1,245 @@
+from qiskit_nature.drivers import PySCFDriver
+from qiskit_nature.problems.second_quantization.electronic import ElectronicStructureProblem
+from qiskit_nature.mappers.second_quantization import ParityMapper, BravyiKitaevMapper, JordanWignerMapper
+from qiskit_nature.converters.second_quantization.qubit_converter import QubitConverter
+from qiskit_nature.circuit.library import HartreeFock
+from qiskit.circuit.library import TwoLocal
+from qiskit_nature.circuit.library import UCCSD, PUCCD, SUCCD
+from qiskit import Aer
+from qiskit.algorithms.optimizers import COBYLA, L_BFGS_B, SPSA, SLSQP
+from qiskit_nature.algorithms.ground_state_solvers.minimum_eigensolver_factories import NumPyMinimumEigensolverFactory
+from qiskit_nature.algorithms.ground_state_solvers import GroundStateEigensolver
+import numpy as np 
+from qiskit.algorithms import VQE
+from IPython.display import display, clear_output
+from qiskit.transpiler import PassManager
+from qiskit.transpiler.passes import Unroller
+
+
+###Part 1
+molecule = "H .0 .0 .0; H .0 .0 0.739"
+#molecule = "Li .0 .0 .0; H .0 .0 1.5474"
+driver = PySCFDriver(atom=molecule)
+qmolecule = driver.run()
+
+###Tutorial
+n_el = qmolecule.num_alpha + qmolecule.num_beta
+n_mo = qmolecule.num_molecular_orbitals
+n_so = 2 * qmolecule.num_molecular_orbitals
+n_q = 2* qmolecule.num_molecular_orbitals
+e_nn = qmolecule.nuclear_repulsion_energy
+
+###Part 2
+problem = ElectronicStructureProblem(driver)
+
+# Generate the second-quantized operators
+second_q_ops = problem.second_q_ops()
+
+# Hamiltonian
+main_op = second_q_ops[0]
+
+###Part 3
+# Setup the mapper and qubit converter
+mapper_type = 'JordanWignerMapper'
+
+if mapper_type == 'ParityMapper':
+    mapper = ParityMapper()
+elif mapper_type == 'JordanWignerMapper':
+    mapper = JordanWignerMapper()
+elif mapper_type == 'BravyiKitaevMapper':
+    mapper = BravyiKitaevMapper()
+
+converter = QubitConverter(mapper=mapper, two_qubit_reduction=False)
+
+# The fermionic operators are mapped to qubit operators
+num_particles = (problem.molecule_data_transformed.num_alpha,
+             problem.molecule_data_transformed.num_beta)
+qubit_op = converter.convert(main_op, num_particles=num_particles)
+
+###Part 4
+num_particles = (problem.molecule_data_transformed.num_alpha,
+             problem.molecule_data_transformed.num_beta)
+num_spin_orbitals = 2 * problem.molecule_data_transformed.num_molecular_orbitals
+init_state = HartreeFock(num_spin_orbitals, num_particles, converter)
+print(init_state)
+
+###Part 5
+# Choose the ansatz
+ansatz_type = "TwoLocal"
+
+# Parameters for q-UCC antatze
+num_particles = (problem.molecule_data_transformed.num_alpha,
+             problem.molecule_data_transformed.num_beta)
+num_spin_orbitals = 2 * problem.molecule_data_transformed.num_molecular_orbitals
+
+# Put arguments for twolocal
+if ansatz_type == "TwoLocal":
+    # Single qubit rotations that are placed on all qubits with independent parameters
+    rotation_blocks = ['ry', 'rz']
+    # Entangling gates
+    entanglement_blocks = 'cx'
+    # How the qubits are entangled 
+    entanglement = 'full'
+    # Repetitions of rotation_blocks + entanglement_blocks with independent parameters
+    repetitions = 3
+    # Skip the final rotation_blocks layer
+    skip_final_rotation_layer = True
+    ansatz = TwoLocal(qubit_op.num_qubits, rotation_blocks, entanglement_blocks, reps=repetitions, 
+                      entanglement=entanglement, skip_final_rotation_layer=skip_final_rotation_layer)
+    # Add the initial state
+    ansatz.compose(init_state, front=True, inplace=True)
+elif ansatz_type == "UCCSD":
+    ansatz = UCCSD(converter,num_particles,num_spin_orbitals,initial_state = init_state)
+elif ansatz_type == "PUCCD":
+    ansatz = PUCCD(converter,num_particles,num_spin_orbitals,initial_state = init_state)
+elif ansatz_type == "SUCCD":
+    ansatz = SUCCD(converter,num_particles,num_spin_orbitals,initial_state = init_state)
+elif ansatz_type == "Custom":
+    # Example of how to write your own circuit
+    from qiskit.circuit import Parameter, QuantumCircuit, QuantumRegister
+    # Define the variational parameter
+    theta = Parameter('a')
+    n = qubit_op.num_qubits
+    # Make an empty quantum circuit
+    qc = QuantumCircuit(qubit_op.num_qubits)
+    qubit_label = 0
+    # Place a Hadamard gate
+    qc.h(qubit_label)
+    # Place a CNOT ladder
+    for i in range(n-1):
+        qc.cx(i, i+1)
+    # Visual separator
+    qc.barrier()
+    # rz rotations on all qubits
+    qc.rz(theta, range(n))
+    ansatz = qc
+    ansatz.compose(init_state, front=True, inplace=True)
+
+print(ansatz)
+
+###Part 6
+backend = Aer.get_backend('statevector_simulator')
+
+###Part 7
+optimizer_type = 'COBYLA'
+
+# You may want to tune the parameters 
+# of each optimizer, here the defaults are used
+if optimizer_type == 'COBYLA':
+    optimizer = COBYLA(maxiter=500)
+elif optimizer_type == 'L_BFGS_B':
+    optimizer = L_BFGS_B(maxfun=500)
+elif optimizer_type == 'SPSA':
+    optimizer = SPSA(maxiter=500)
+elif optimizer_type == 'SLSQP':
+    optimizer = SLSQP(maxiter=500)
+
+###Part 8
+def exact_diagonalizer(problem, converter):
+    solver = NumPyMinimumEigensolverFactory()
+    calc = GroundStateEigensolver(converter, solver)
+    result = calc.solve(problem)
+    return result
+
+result_exact = exact_diagonalizer(problem, converter)
+exact_energy = np.real(result_exact.eigenenergies[0])
+print("Exact electronic energy", exact_energy)
+print(result_exact)
+
+# The targeted electronic energy for H2 is -1.85336 Ha
+# Check with your VQE result.
+
+###Part 9
+# Print and save the data in lists
+def callback(eval_count, parameters, mean, std):  
+    # Overwrites the same line when printing
+    display("Evaluation: {}, Energy: {}, Std: {}".format(eval_count, mean, std))
+    clear_output(wait=True)
+    counts.append(eval_count)
+    values.append(mean)
+    params.append(parameters)
+    deviation.append(std)
+
+counts = []
+values = []
+params = []
+deviation = []
+
+# Set initial parameters of the ansatz
+# We choose a fixed small displacement 
+# So all participants start from similar starting point
+try:
+    initial_point = [0.01] * len(ansatz.ordered_parameters)
+except:
+    initial_point = [0.01] * ansatz.num_parameters
+
+algorithm = VQE(ansatz,
+                optimizer=optimizer,
+                quantum_instance=backend,
+                callback=callback,
+                initial_point=initial_point)
+
+result = algorithm.compute_minimum_eigenvalue(qubit_op)
+
+print(result)
+
+
+###Part Scoring
+# Unroller transpile your circuit into CNOTs and U gates
+pass_ = Unroller(['u', 'cx'])
+pm = PassManager(pass_)
+ansatz_tp = pm.run(ansatz)
+cnots = ansatz_tp.count_ops()['cx']
+score = cnots
+
+accuracy_threshold = 4.0 # in mHa
+energy = result.optimal_value
+
+if ansatz_type == "TwoLocal":
+    result_dict = {
+        'optimizer': optimizer.__class__.__name__,
+        'mapping': converter.mapper.__class__.__name__,
+        'ansatz': ansatz.__class__.__name__,
+        'rotation blocks': rotation_blocks,
+        'entanglement_blocks': entanglement_blocks,
+        'entanglement': entanglement,
+        'repetitions': repetitions,
+        'skip_final_rotation_layer': skip_final_rotation_layer,
+        'energy (Ha)': energy,
+        'error (mHa)': (energy-exact_energy)*1000,
+        'pass': (energy-exact_energy)*1000 <= accuracy_threshold,
+        '# of parameters': len(result.optimal_point),
+        'final parameters': result.optimal_point,
+        '# of evaluations': result.optimizer_evals,
+        'optimizer time': result.optimizer_time,
+        '# of qubits': int(qubit_op.num_qubits),
+        '# of CNOTs': cnots,
+        'score': score}
+else:
+    result_dict = {
+        'optimizer': optimizer.__class__.__name__,
+        'mapping': converter.mapper.__class__.__name__,
+        'ansatz': ansatz.__class__.__name__,
+        'rotation blocks': None,
+        'entanglement_blocks': None,
+        'entanglement': None,
+        'repetitions': None,
+        'skip_final_rotation_layer': None,
+        'energy (Ha)': energy,
+        'error (mHa)': (energy-exact_energy)*1000,
+        'pass': (energy-exact_energy)*1000 <= accuracy_threshold,
+        '# of parameters': len(result.optimal_point),
+        'final parameters': result.optimal_point,
+        '# of evaluations': result.optimizer_evals,
+        'optimizer time': result.optimizer_time,
+        '# of qubits': int(qubit_op.num_qubits),
+        '# of CNOTs': cnots,
+        'score': score}
+
+print(result_dict['score'])
+
+#print(n_el)
+#print(n_mo)
+#print(n_so)
+#print(n_q)
+#print(e_nn)
